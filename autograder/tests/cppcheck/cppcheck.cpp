a275991fb3f806f7fb1468564dcac498c5aef38d
RubenRivas <rudrivas@hotmail.com>
2023-01-15 19:46:43 -0500

    update matrix exercises

--- autograder/tests/cppcheck/cppcheck.cpp
+++ autograder/tests/cppcheck/cppcheck.cpp
@@ -3,6 +3,74 @@
 #include <algorithm>
 using namespace std;
 
+static void question_1_1() {
+    random_device rd;
+    utec::matrix m1(10, 20);
+    utec::matrix m2(30, 40);
+    uniform_int_distribution<int> dis(0, 100);
+    for (size_t i = 0; i < m1.rows(); ++i) {
+        for (size_t j = 0; j < m1.cols(); ++j)
+            m1(i, j) = dis(rd);
+    }
+    utec::matrix m3 = m1;
+    for (size_t i = 0; i < m2.rows(); ++i) {
+        for (size_t j = 0; j < m2.cols(); ++j)
+            m2(i, j) = dis(rd);
+    }
+    cout << m1.rows() << " " << m1.cols() << endl;
+    cout << m2.rows() << " " << m2.cols() << endl;
+    swap(m1, m2);
+    cout << m1.rows() << " " << m1.cols() << endl;
+    cout << m2.rows() << " " << m2.cols() << endl;
+}
+
+static void question_1_2() {
+    size_t rows = 0, cols = 0;
+    cin >> rows >> cols;
+    utec::matrix m1(rows, cols);
+    for (size_t i = 0; i < m1.rows(); ++i) {
+        for (size_t j = 0; j < m1.cols(); ++j) {
+            cin >> m1(i, j);
+        }
+    }
+    cin >> rows >> cols;
+    utec::matrix m2(rows, cols);
+    for (size_t i = 0; i < m2.rows(); ++i) {
+        for (size_t j = 0; j < m2.cols(); ++j)
+            cin >> m2(i, j);
+    }
+    utec::matrix m3 = m1;
+    cout << m1.rows() << " " << m1.cols() << endl;
+    cout << m2.rows() << " " << m2.cols() << endl;
+    std::swap(m1, m2);
+    m2(0, 0) = 99;
+    cout << m1.rows() << " " << m1.cols() << endl;
+    cout << m2.rows() << " " << m2.cols() << endl;
+    cout << m1 << endl << m2 << endl << m3 <<endl;
+}
+
+static void question_1_3() {
+    random_device rd;
+    utec::matrix m1(8, 9);
+    utec::matrix m2(4, 5);
+    uniform_int_distribution<int> dis(0, 100);
+    for (size_t i = 0; i < m1.rows(); ++i)
+        for (size_t j = 0; j < m1.cols(); ++j)
+            m1(i, j) = dis(rd);
+    // Copia
+    utec::matrix m3 = m1;
+    for (size_t i = 0; i < m2.rows(); ++i)
+        for (size_t j = 0; j < m2.cols(); ++j)
+            m2(i, j) = dis(rd);
+    // Comparando matrices
+    cout << boolalpha << (m1 == m2) << endl;
+    cout << boolalpha << (m3 == m1) << endl;
+    cout << boolalpha << (m2 != m3) << endl;
+}
+
 int main() {
+    question_1_1();
+    question_1_2();
+    question_1_3();
     return 0;
 }
