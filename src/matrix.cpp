76ce6fe223428f6b1710146f826d30bfe28b57a2
RubenRivas <rudrivas@hotmail.com>
2023-01-15 19:39:23 -0500

    update matrix exercises

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -46,16 +46,11 @@ utec::matrix &utec::matrix::operator=(utec::matrix &&other) noexcept {
 }
 
 utec::matrix::~matrix() {
-    delete [] data;
+//    delete [] data;
 }
 
-TYPE& utec::matrix::operator() (SIZE_TYPE row, SIZE_TYPE col) {
-    return data[row*n_cols + col];
-}
-
-TYPE utec::matrix::operator() (SIZE_TYPE row, SIZE_TYPE col) const {
-    return data[row*n_cols + col];
-}
+TYPE& utec::matrix::operator() (SIZE_TYPE row, SIZE_TYPE col) { return data[row*n_cols + col]; }
+TYPE utec::matrix::operator() (SIZE_TYPE row, SIZE_TYPE col) const { return data[row*n_cols + col]; }
 
 SIZE_TYPE utec::matrix::cols() const { return n_cols; }
 SIZE_TYPE utec::matrix::rows() const { return n_rows; }
@@ -63,7 +58,7 @@ SIZE_TYPE utec::matrix::rows() const { return n_rows; }
 std::ostream& utec::operator<<(std::ostream &os, const utec::matrix &m) {
     for (size_t i = 0; i < m.n_rows; ++i) {
         for (size_t j = 0; j < m.n_cols; ++j)
-            os << std::setw(4) << m.data[i*m.n_cols + j];
+            os << std::setw(4) << m(i, j);
         os << std::endl;
     }
     return os;
@@ -76,7 +71,7 @@ utec::matrix utec::matrix::operator+(const utec::matrix &other) {
     auto result = *this;
     for (size_t i = 0; i < result.n_rows; ++i)
         for (size_t j = 0; j < result.n_cols; ++j)
-            result.data[i*result.n_cols +j] += other.data[i*result.n_cols +j];
+            result(i, j) += other(i, j);
     return result;
 }
 
@@ -90,8 +85,8 @@ utec::matrix utec::matrix::operator*(const utec::matrix &other) {
         for (size_t j = 0; j < result.n_cols; ++j) {
             TYPE item = TYPE {};
             for (size_t k = 0; k < n_cols; ++k)
-                item += data[i*n_cols + k] * other.data[k*other.n_cols + j];
-            result.data[i*result.n_cols + j] = item;
+                item += (*this)(i, k) * other(k, j);
+            result(i, j) = item;
         }
     return result;
 }
@@ -105,18 +100,18 @@ utec::matrix utec::matrix::operator*=(TYPE scalar) {
 bool utec::matrix::operator==(const utec::matrix &other) const {
     if (n_rows != other.n_rows || n_cols != other.n_cols) return false;
     auto result = true;
-    for (size_t i = 0; i < n_rows; ++i)
+    for (size_t i = 0; i < n_rows && result; ++i)
         for (size_t j = 0; j < n_cols && result; ++j)
-            result = data[i*n_cols + j] == other.data[i*n_cols + j];
+            result = (*this)(i, j) == other(i, j);
     return result;
 }
 
 bool utec::matrix::operator!=(const utec::matrix &other) {
     if (n_rows != other.n_rows || n_cols != other.n_cols) return true;
     auto result = false;
-    for (size_t i = 0; i < n_rows; ++i)
+    for (size_t i = 0; i < n_rows && !result; ++i)
         for (size_t j = 0; j < n_cols && !result; ++j)
-            result = data[i*n_cols + j] != other.data[i*n_cols + j];
+            result =  (*this)(i, j) != other(i, j);
     return result;
 }
 
@@ -124,7 +119,7 @@ utec::matrix utec::operator*(const utec::matrix &m, TYPE scalar) {
     auto result = m;
     for (size_t i = 0; i < m.n_rows; ++i)
         for (size_t j = 0; j < m.n_cols; ++j)
-            result.data[i*m.n_cols + j] = m.data[i*m.n_cols + j] * scalar;
+            result(i, j) = m(i, j) * scalar;
     return result;
 }
 
